9fa1862dd66d6628f36ccc7c43516f148eb84260
ftotherank <102555981+ftotherank@users.noreply.github.com>
2026-03-27 13:18:05 +0300

    fighting with errors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5001
 app.use(cors({ 
   origin: [
     'http://localhost:5173',
-     'https://afya-ochre.vercel.app/login'
+     'https://afya-ochre.vercel.app'
   ], 
   credentials: true 
 }))
